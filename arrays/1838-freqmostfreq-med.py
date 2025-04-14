9b00cb9b5a08a98aba61027b0207855de2b6bd7b
The Lancelot <nkcubing1@gmail.com>
2025-04-14 17:07:47 +0530

    Sliding window

--- arrays/1838-freqmostfreq-med.py
+++ arrays/1838-freqmostfreq-med.py
@@ -6,28 +6,40 @@ class Solution:
         nums.sort()
         ans=1
 
-# x-y
-# x-z
-
-# y-z
         #bruteforce O(N^2) TLE for 52/73 test cases
-        for i in reversed(range(len(nums))):
-            dummy=k
-            inner_ans=1
+        # for i in reversed(range(len(nums))):
+        #     dummy=k
+        #     inner_ans=1
 
-            for j in reversed(range(0,i)):
+        #     for j in reversed(range(0,i)):
 
-                diff=nums[i]-nums[j]
+        #         diff=nums[i]-nums[j]
 
                 
-                if diff<=dummy:
-                    dummy=dummy-diff
-                    inner_ans+=1
+        #         if diff<=dummy:
+        #             dummy=dummy-diff
+        #             inner_ans+=1
                 
-            if inner_ans>ans:
-                ans=inner_ans
+        #     if inner_ans>ans:
+        #         ans=inner_ans
             
-        return ans
+        # return ans
     
-        # count=Counter(nums)
-        # print(count)
+        # Sliding Window Final Approach
+        #my initial approach of take the entire window and keep decreasing it failed unfortunately
+        
+        l=r=0
+        current_window_sum=0
+        ans=0
+        while r<len(nums):
+            current_window_sum+=nums[r] 
+        
+            while((r-l+1)*nums[r]-current_window_sum)>k:
+                print("in")
+                current_window_sum-=nums[l]
+                l+=1
+
+            ans=max(ans,r-l+1)
+            r+=1
+
+        return ans
